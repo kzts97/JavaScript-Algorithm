c8dc9951513451238d6b12be9ceccbf57fb3f34d
Han jun Kang <zkwmxhtl97@gmail.com>
2024-04-05 22:07:44 +0900

    [level 0] Title: 분수의 덧셈, Time: 0.19 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120808. 분수의 덧셈/README.md
+++ 프로그래머스/0/120808. 분수의 덧셈/README.md
@@ -1,10 +1,10 @@
 # [level 0] 분수의 덧셈 - 120808 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120808) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120808?language=javascript) 
 
 ### 성능 요약
 
-메모리: 33.4 MB, 시간: 0.20 ms
+메모리: 33.5 MB, 시간: 0.19 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 04월 05일 22:00:12
+2024년 04월 05일 22:07:42
 
 ### 문제 설명
 
--- 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.js
+++ 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.js
@@ -1,25 +1,28 @@
 function solution(denum1, num1, denum2, num2) {
     let denum = denum1 * num2 + denum2 * num1;
     let num = num1 * num2;
-
-    let minNumber; //작은수 구하기
+    
+    //작은수 구하기
+    let minNumber; 
     if(denum < num){ //분모< 분자 작을때 
         minNumber = denum;
     }else{
         minNumber = num;
     }
-    
+    //나누어 떨어지는것 ->나머지가 0 
     while (true) {
-        if(denum % minNumber === 0 ){
-            if(num % minNumber === 0){
+        if(denum % minNumber === 0 ){ //분자를 나누어떨어지게
+            if(num % minNumber === 0){ //분모를~~
         //min number 은 최대공약수
             return [denum / minNumber, num / minNumber] //기약분수 
             }
         }
+        // 1줄이면 최종적으로 1이나올때까지
 minNumber = minNumber -1;
     }
 }
     //1.분모를 덧셈
+
     //2.분자,분모의 최대공약수로 나누기.
     //2-1.분자,분모 중 작은 수 찾기.
     //2-2.작은수를 분자,분모로 나누어보기.
