dffa3e0efb023d647731bd438b0f2ce37232ac7c
Han jun Kang <zkwmxhtl97@gmail.com>
2024-04-05 22:00:13 +0900

    [level 0] Title: 분수의 덧셈, Time: 0.20 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.js
+++ 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.js
@@ -1,13 +1,30 @@
-// 최대 공약수 구하기
-function cal_gcd(a, b) {
-    return a % b === 0 ? b : cal_gcd(b, a % b)
-}
-
 function solution(denum1, num1, denum2, num2) {
     let denum = denum1 * num2 + denum2 * num1;
     let num = num1 * num2;
-    let gcd = cal_gcd(denum, num);
+
+    let minNumber; //작은수 구하기
+    if(denum < num){ //분모< 분자 작을때 
+        minNumber = denum;
+    }else{
+        minNumber = num;
+    }
     
-    // 최대 공약수를 분자 분모에 나누고 배열에 넣기
-    return [denum / gcd, num / gcd]
+    while (true) {
+        if(denum % minNumber === 0 ){
+            if(num % minNumber === 0){
+        //min number 은 최대공약수
+            return [denum / minNumber, num / minNumber] //기약분수 
+            }
+        }
+minNumber = minNumber -1;
+    }
 }
+    //1.분모를 덧셈
+    //2.분자,분모의 최대공약수로 나누기.
+    //2-1.분자,분모 중 작은 수 찾기.
+    //2-2.작은수를 분자,분모로 나누어보기.
+    //2-2-1. 둘다 나누어 떨어지면 그 나눈수가 최대공약수
+    //2-2-2.안 나누어떨어지면 작은수를 1줄이고 2-2로돌아가기.
+
+
+
